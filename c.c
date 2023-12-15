39e53cb5382b3d584a6d91306dde148f602410ef
jo <qwqs78@naver.com>
2023-12-15 16:10:33 +0900

    12_15

--- c.c
+++ c.c
@@ -3,5 +3,17 @@
 int main(void)
 {
     printf("Hello world\n");
+
+    int num1, num2;
+
+    printf("두 수를 입력하세요 : \n");
+    printf("%d,%d", &num1,&num2);
+
+    int sum = num1+num2;
+    printf(sum);
+    
+
+
     return 0;
+
 }
